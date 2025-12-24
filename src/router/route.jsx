366efe0ebd547137c8a8414b5bc3a0520e7c7564
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-12-24 19:23:41 +0600

    setup project and navbar component design

---
+++ src/router/route.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router';
+import App from '../App';
+import HomePage from '../page/HomePage';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+    children: [{ index: true, Component: HomePage }],
+  },
+]);
