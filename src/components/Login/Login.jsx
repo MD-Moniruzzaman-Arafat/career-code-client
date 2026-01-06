35eca9a6b1c445bd7d59e2baa04d61a2dc742cb4
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-01-06 11:22:16 +0600

    login system implement

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,88 @@
-import { Link } from 'react-router';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { Bounce, toast } from 'react-toastify';
+import useAuth from '../../hook/useAuth';
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const navigation = useNavigate();
+  const { user, setUser, authError, setAuthError, logInUser } = useAuth();
+  const errorNotify = () =>
+    toast.error(authError, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+      transition: Bounce,
+    });
+  const successNotify = () =>
+    toast.success('Login successful!', {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+      transition: Bounce,
+    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Login user:', formData);
+    try {
+      const response = await logInUser(formData.email, formData.password);
+      const loginUser = response.user;
+      if (loginUser) {
+        navigation('/');
+      }
+      console.log(loginUser);
+      successNotify();
+    } catch (error) {
+      setAuthError(error.message);
+      errorNotify();
+    }
+  };
   return (
     <>
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <div className="card-body">
-          <fieldset className="fieldset">
+          <form onSubmit={handleSubmit} className="fieldset">
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              onChange={handleChange}
+              value={formData.email}
+              type="email"
+              className="input"
+              placeholder="Email"
+            />
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              onChange={handleChange}
+              value={formData.password}
+              type="password"
+              className="input"
+              placeholder="Password"
+            />
             {/* <div>
                   <a className="link link-hover">Forgot password?</a>
                   </div> */}
             <button className="btn btn-neutral mt-4">Login</button>
-          </fieldset>
+          </form>
           <div className="text-center">
             <small>
               don't have an account? <Link to="/register-page">Register</Link>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,17 +1,47 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { useEffect, useState } from 'react';
 import auth from '../../firebase.config';
+import Loading from '../components/Loading/Loading';
 import { AuthContext } from '../context';
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [authError, setAuthError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  const logInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-  const authInfo = { user, setUser, authError, setAuthError, createUser };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log('Auth State Changed:', currentUser);
+      currentUser ? setLoading(false) : setLoading(false);
+    });
+    return () => unsubscribe();
+  });
+
+  const authInfo = {
+    setLoading,
+    user,
+    setUser,
+    authError,
+    setAuthError,
+    createUser,
+    logInUser,
+  };
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
